426e1d8f564f1e140e9df21348d50f9cd9413c3c
A-Safarii <86190642+A-Safarii@users.noreply.github.com>
2021-07-03 16:08:50 +0430

    Add files via upload

---
+++ Mixed Messages.js
@@ -0,0 +1,164 @@
+
+const Aries = {
+    advice: ["Trust your abilities.",
+    "Avoid risky transactions. Do not get carried away by erroneous information, or from dubious sources.",
+    "Cultivate the necessary practical skills to boost your career or entrepreneurship.",
+    "If you need advice, turn to your loved ones.",
+    "Do not lose hope. Bury the past, and walk into the present.",
+    "Take time to reflect on your goals, ambitions, and future career.",
+    "Efficiently manage your resources and shared assets. Financial stability is very important.",
+    "Honesty will be essential for the development of family and sentimental relationships.",
+    "Be grateful for what you have. Academic awards and unexpected opportunities will arise. Honest work pays off.",
+    "Feelings of conformity and helplessness must be overcome. Remember: every obstacle is an opportunity for growth. You have what it takes to win.",
+    "Break old thought patterns. Believe in your dreams. Avoid victimization.",
+    "Let go of old customs and beliefs, to regain your personal power, and enter a new stage of your life."],
+    characteristic: ['Impulsive', 'Initiative', 'Courageous', 'Determined', 'Emotional', 'Competetive', 'Naive', 'Trustworthy', 'Independent', 'Protective']
+}
+
+const Leo = {
+    advice: ["Redefine your scale of values; creates a new structure for financial management; and reflect on how you use your talents and abilities.",
+    "Diversify your sources of income, and explore investment options (conservative) that provide profitability.",
+    "Interact with family and friends as a channel for the inspiration of new ideas and projects.",
+    "Take time to reflect on the past, and the future. Identify the boundaries of the comfort zone.",
+    "Rely on the support of friends and loved ones as it will be essential to overcome setbacks and find internal balance.",
+    "You will become frustrated due to circumstances associated with the work environment. Change your approach strategy, to obtain the solution and a renewed perspective on life.",
+    "Avoid exaggerated expectations, within the relationship, and open the door to (really) know your partner.",
+    "Through the exchange of experiences and shared goals, you will achieve a higher level of intimacy in the relationship.",
+    "You will find friendship with people of another culture or nationality. Be discreet and patient with your plans.",
+    "If you feel trapped by the monotony of everyday tasks, don’t hesitate to redefine your career goals; consult with a vocational coach; and acquire new skills, which propel you in the search for challenges.",
+    "You will have successful businesses and projects with friends and colleagues. Opportunities will arise unexpectedly. Aim towards your dreams.",
+    "Share your joy, love and knowledge with those willing to receive."],
+    characteristic: ["Ambitious", "Confident", "Charismatic", "Attention-seeking", "Generous", "Warm", "Authoritative", "Strong leaders", "Egotistical", "Dignified"]
+}
+
+const Cancer = {
+    advice: ["Connect with the voice of inner wisdom. Pay attention to details, and become aware of the process for structuring your plans.",
+    "As far as possible, avoid applying for new credits, which can threaten your ability to pay in the long term.",
+    "Don’t hold on to the past. Define your goals.",
+    "Caution with family businesses (possibility of dubious handling). Trust your ingenuity to find creative and unexpected solutions. Don’t lose faith.",
+    "Motivate yourself to fight for your dreams.",
+    "Defend your position at work with certainty and good sense, without letting yourself be dominated by pride (ego).",
+    "Every relationship requires a period of adjustment, and dedication, to overcome initial difficulties. Spend time getting to know your partner.",
+    "If you hold on to a love from the past, or old beliefs, you can hardly move forward in the present relationship, or open yourself to new opportunities. There will be changes in the management of family assets.",
+    "The problems will not end alone, so you must take action. Alliances and associations will give you the boost you need, to move towards the goal.",
+    "You will face dissatisfaction and burnout episodes. It will be necessary to close the cycle (thanking the learning received), and embark on a new path that brings you closer to your vocation and purpose.",
+    "Advice from loyal friends is welcome. Remember that you are not infallible.",
+    "Aligning the reason and the heart will strengthen the inner balance. We are all one with the Divine."],
+    characteristic: ['Loyal', 'Intuitive', 'Protective', 'Emotional', 'Helpful', 'Sensitive', 'Imaginative', 'Blunt', 'Social', 'Empathetic']
+}
+
+const Scorpio = {
+    advice: ["Designing strategic planning will allow you to delegate responsibilities, and free yourself from burdens that do not correspond to you, to achieve the proposed objectives.",
+    "Financial problems can be solved successfully, if you manage funds wisely and wisely.",
+    "More experienced people will offer valuable advice to help you change your perspective and face your fears.",
+    "Remember that after the storm, calm comes.",
+    "Possibility of meeting someone special. To strengthen ties, you need to be honest with your feelings.",
+    "Don’t be afraid to defend yourself against unhealthy comments and unfair competition in the workplace.",
+    "Situations in your relationship that have to be resolved need to be clarified. To reach a true understanding, both parties must dedicate the same effort and intention.",
+    "Confidence and enjoyment of sexuality, as instruments to strengthen ties in the couple relationship.",
+    "Motivate yourself to achieve your goals.",
+    "Time and opportunity are in your favor. Rushing deadlines can generate setbacks in the professional area. Be patient.",
+    "There will be interesting professional proposals, by friends and colleagues. Participation in group activities will boost personal development.",
+    "Appeal to your ability to adapt. Holding on to old ideas will hinder the evolutionary process."],
+    characteristic: ['Loyal', 'Passionate', 'Resourceful', 'Observant', 'Dynamic', 'Persuasive', 'Honest', 'Mature', 'Balanced', 'Dedicated']
+}
+
+const Libra = {
+    advice: ["In the face of obstacles, define the strategies that will bring you closer to the goal.",
+    "Take advantage of periods of calm, to restructure personal finances; the administration of assets; available sources of income; and the economic objectives, in the short and medium term.",
+    "Present your ideas confidently and effectively. Avoid sarcasm.",
+    "Possibility of pregnancy, or of celebrating a birth in the clan. Share love and happiness.",
+    "Ask yourself if you are making proper use of your talents and abilities. Beware of victimization.",
+    "Your efforts will be rewarded. Boost your ideas and projects.",
+    "Feel free to ask for support, to gain a new perspective, overcome obstacles, and heal.",
+    "It is necessary to break with negative thoughts and limiting situations, to conquer challenges.",
+    "You will have excellent prospects in academic projects and international business. Avoid engaging in malicious backbiting. Verify in detail contracts and agreements, before signing them.",
+    "Remember that your decisions affect both yourself and the people around you.",
+    "You will face conflicts with friends and work groups. Defend your point of view, presenting arguments with maturity and honesty.",
+    "Stay away from intrigue, and embrace the principles of your personal ethics."],
+    characteristic: ['Forgiving', 'Fair', 'Charming', 'Selfless', 'Social', 'Indecisive', 'Unreliable', 'Non-confrontational', 'Clever', 'Open-minded']
+}
+
+const Taurus = {
+    advice: ["Reassess your personal goals. Be confident to be yourself, regardless of the circumstances.",
+    "Manage your income wisely and intelligently. Avoid incurring new debts, or financial commitments.",
+    "Put your multiple talents to proper use.",
+    "Be firm in situations of family conflict. Stand up for principles. Be aware of your purpose, and you will find the solution.",
+    "Beyond worry and anguish, direct your mind to the search for practical solutions. Never lose your optimism.",
+    "Your efforts will lead to success. Persevere.",
+    "The wounds of the past prevent progress in the relationships of the present. Close the loop, and get ready for new experiences.",
+    "The Universe is aligned with your purpose. The love you give will return multiplied.",
+    "Avoid clinging to pride, and request support to overcome obstacles. Remember the well-known African proverb: 'If you want to go fast, walk alone; if you want to go far, go together'.",
+    "You will reach a new stage in the professional field. Receive opportunities with love and gratitude to raise your vibration.",
+    "Take advantage of events to your advantage, to cultivate new friendships, enter groups and associations, and expand your network of professional contacts.",
+    "We can live in the material world without losing our spiritual consciousness. Wearing a “shell” of skepticism only makes it difficult to access the miracles that are waiting for you."],
+    characteristic: ['Persistent', 'Patient', 'Tough', 'Focused', 'Sensual', 'Realistic', 'Steady', 'Loving', 'Materialistic', 'Lazy']
+}
+
+//Fix user input
+//let userInput = prompt('Enter your Star Sign: ');
+//userInput.toLowerCase();
+
+let char1 = Math.floor(Math.random() * 10);
+let char2 = Math.floor(Math.random() * 10);
+let char3 = Math.floor(Math.random() * 10);
+
+while (char1 === char2 || char1 === char3 || char2 === char3) {
+    char1 = Math.floor(Math.random() * 10);
+    char2 = Math.floor(Math.random() * 10);
+    char3 = Math.floor(Math.random() * 10);
+}
+
+let userInput = 'leo';
+
+//Add true random
+switch(userInput) {
+    case 'aries':
+        console.log('Advice: ');
+        console.log(Aries.advice[Math.floor(Math.random() * Aries.advice.length)]);
+        console.log('Characteristics of an Aries: ');
+        console.log(Aries.characteristic[char1]);
+        console.log(Aries.characteristic[char2]);
+        console.log(Aries.characteristic[char3]);
+        break;
+    case 'leo':
+        console.log('Advice: ');
+        console.log(Leo.advice[Math.floor(Math.random() * Leo.advice.length)]);
+        console.log('Characteristics of a Leo: ');
+        console.log(Leo.characteristic[char1]);
+        console.log(Leo.characteristic[char2]);
+        console.log(Leo.characteristic[char3]);
+        break;
+    case 'cancer':
+        console.log('Advice: ');
+        console.log(Cancer.advice[Math.floor(Math.random() * Cancer.advice.length)]);
+        console.log('Characteristics of a Cancer: ');
+        console.log(Cancer.characteristic[char1]);
+        console.log(Cancer.characteristic[char2]);
+        console.log(Cancer.characteristic[char3]);
+        break;
+    case 'scorpio':
+        console.log('Advice: ');
+        console.log(Scorpio.advice[Math.floor(Math.random() * Scorpio.advice.length)]);
+        console.log('Characteristics of a Scorpio: ');
+        console.log(Scorpio.characteristic[char1]);
+        console.log(Scorpio.characteristic[char2]);
+        console.log(Scorpio.characteristic[char3]);
+        break;
+    case 'libra':
+        console.log('Advice: ');
+        console.log(Libra.advice[Math.floor(Math.random() * Libra.advice.length)]);
+        console.log('Characteristics of a Libra: ');
+        console.log(Libra.characteristic[char1]);
+        console.log(Libra.characteristic[char2]);
+        console.log(Libra.characteristic[char3]);
+        break;
+    case 'taurus':
+        console.log('Advice: ');
+        console.log(Taurus.advice[Math.floor(Math.random() * Taurus.advice.length)]);
+        console.log('Characteristics of a Taurus: ');
+        console.log(Taurus.characteristic[char1]);
+        console.log(Taurus.characteristic[char2]);
+        console.log(Taurus.characteristic[char3]);
+        break;
+}
